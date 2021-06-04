2d2114a94606fba3cb15339df1ed5a87534126e4
YU.eucalyptus <yukarixxx1220@icloud.com>
2021-06-04 21:09:40 +0900

    2回目

--- app/src/main/java/jp/techacademy/yukari/kotlinlog/Human.kt
+++ app/src/main/java/jp/techacademy/yukari/kotlinlog/Human.kt
@@ -2,18 +2,26 @@ package jp.techacademy.yukari.kotlinlog
 
 import android.util.Log
 
+const val MY_TAG = "kotlintest"
+
 open class Human: Animal, Thinkable {
 
     constructor(name: String, age: Int, hobby: String): super(name, age, hobby){
     }
     override fun say() {
+     //   var name:String
+     //   var age:Int
         Log.d("kotlintest", "私の名前は" + this.name + "です。")
         Log.d("kotlintest", "年は" + this.age + "歳です。")
     }
 
-    val human = Human(name, age, hobby)
+    fun MyTag(): String{
+        return "[" + MY_TAG + "]"
+    }
+
 
     override fun think(){
+      //  var hobby: String
         Log.d("kotlintest", "私は" + this.hobby + "について考える")
     }
 }
--- app/src/main/java/jp/techacademy/yukari/kotlinlog/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yukari/kotlinlog/MainActivity.kt
@@ -4,14 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var human = Human ("ゆかり", 25, hobby = "Netflix")
+        val human = Human ("ゆかり", 25, hobby = "Netflix")
 
-      human.think()
+        human.say()
+        Log.d("kotlintest", "私の名前は" + human.name + "です。")
+        Log.d("kotlintest", "年は" + human.age + "歳です。")
+
+        val human2 = Human("Kate", 20, "DisneyPlus")
+
+        human2.think()
+        Log.d("kotlintest", "私は" + human2.hobby + "について考える")
+        Log.d(MY_TAG, "私は" + human2.hobby + "について考える。")
 
     }
 }
